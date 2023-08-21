a3be02b42933f79283c7f7b50db606dc74a0f361
Gustavo Hammes <sistema.gustavo@gmail.com>
2023-08-21 08:32:03 -0300

    start

---
+++ src/app/Config/Routes.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+/*
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
+// where controller filters or CSRF protection are bypassed.
+// If you don't want to define all routes, please use the Auto Routing (Improved).
+// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
+// $routes->setAutoRoute(false);
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
+$routes->get('/', 'Home::index');
+# www/cdff3467e47eff0530a8465ddda62adf
+$routes->get('cdff3467e47eff0530a8465ddda62adf', 'Cea8409cb2bdb4db7a596696db5619ce::m30651056d1dfec84a0f3363168fa213c');
+# www/start/ae0b08f5adff7b670af91e0993973c3f
+$routes->get('start/ae0b08f5adff7b670af91e0993973c3f', 'B73a0c131581fc59b09e63147a50b03d::d15c53522d44dd1fac5735340d7e31cd');
+
+
+/*
+ * --------------------------------------------------------------------
+ * Additional Routing
+ * --------------------------------------------------------------------
+ *
+ * There will often be times that you need additional routing and you
+ * need it to be able to override any defaults in this file. Environment
+ * based routes is one such time. require() additional route files here
+ * to make that happen.
+ *
+ * You will have access to the $routes object within that file without
+ * needing to reload it.
+ */
+if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
